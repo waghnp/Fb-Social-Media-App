dad0e6bafbd404afd2d52619d09194fb274d5a77
waghnp <nwagh436@gmail.com>
2020-10-29 08:52:51 +0530

    setup

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header=()=>{
+    return( 
+        <div className="header" >
+            <div className="header_left">
+                <img src="https://en.wikipedia.org/wiki/Facebook#/media/File:Facebook_f_logo_(2019).svg" alt="facebook logo"/>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
