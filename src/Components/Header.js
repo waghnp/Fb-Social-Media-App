2f53b09cfdfacebee83a172a6108521510fb0249
waghnp <nwagh436@gmail.com>
2020-10-30 14:08:25 +0530

    main header completed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,65 @@
 import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {Avatar, IconButton} from '@material-ui/core';
+import './Header.css'
 
 const Header=()=>{
     return( 
         <div className="header" >
-            <div className="header_left">
-                <img src="https://en.wikipedia.org/wiki/Facebook#/media/File:Facebook_f_logo_(2019).svg" alt="facebook logo"/>
+            <div className="header__left">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="facebook logo"/>
+            </div>
+            <div className="header__input">
+                <SearchIcon/>
+                <input type="text" name="search" placeholder="Search"/>
+            </div>
+            <div className="header__center">
+                <div className="header__option header__option--active">
+                    <HomeIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <FlagIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <SubscriptionsOutlinedIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <StorefrontOutlinedIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon fontSize="large"/>
+                </div>
+            </div>
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar/>
+                    <h4>Nilesh wagh</h4>
+                </div>
+                <IconButton>
+                    <AddIcon/>
+                </IconButton>
+
+                <IconButton>
+                    <ForumIcon/>
+                </IconButton>
+               
+                <IconButton>
+                    <NotificationsActiveIcon/>
+                </IconButton>
+        
+                <IconButton>
+                    <ExpandMoreIcon/>
+                </IconButton>
+            
             </div>
         </div>
     )
