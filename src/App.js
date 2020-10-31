ab6f3b2d75246321bf18605e7054f19261b998d5
waghnp <nwagh436@gmail.com>
2020-10-31 13:51:52 +0530

    sidebar Front-end completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Components/Header'
+import Sidebar from './Components/Sidebar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
           <Header/>
+          <Sidebar/>
     </div>
   );
 }
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Sidebar.css';
+import SidebarRow from './SidebarRow';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
+import PeopleIcon from '@material-ui/icons/People';
+import ChatIcon from '@material-ui/icons/Chat';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
+// import {useStateValue} from '../StateProvider';
+
+
+const Sidebar=()=>{
+    return(
+        <div className="sidebar" >
+            <SidebarRow src="" title="Nilesh wagh" />
+            <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Information Center" />
+            <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+            <SidebarRow Icon={PeopleIcon} title="Friends" />
+            <SidebarRow Icon={ChatIcon} title="Messenger" />
+            <SidebarRow Icon={StorefrontIcon} title="MarketPlace" />
+            <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+            <SidebarRow Icon={ExpandMoreOutlinedIcon} title="More" />
+        </div>
+    );
+}
+
+export default Sidebar;
